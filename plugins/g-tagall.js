de27c9a661b5470ae39a2860f1da65a676cafb69
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-06 21:42:52 +0700

    p

--- plugins/g-tagall.js
+++ plugins/g-tagall.js
@@ -3,10 +3,10 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants }) => {
                 global.dfail('admin', m, conn)
                 throw false
                 }
-  let teks = `${text ? text : ''}\n\n┌─「 Tag All 」\n`
+  let teks = `${text ? text : ''}\n\n┏━ꕥ〔 *Tag All* 〕ꕥ━⬣\n`
   for (let mem of participants) {
-  teks += `├ @${mem.id.split('@')[0]}\n`}
-  teks += `└────\n`
+  teks += `┃✾ @${mem.id.split('@')[0]}\n`}
+  teks += `┗━━━━ꕥ\n`
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <message>']
