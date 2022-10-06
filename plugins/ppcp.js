1a5da97c8b3df2f171f33a7779cc39beedcc1752
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-06 20:25:19 +0700

    P

--- plugins/ppcp.js
+++ plugins/ppcp.js
@@ -1,21 +1,30 @@
 let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
+
 //https://api.lolhuman.xyz/api/random/ppcouple?apikey=RFK-Rey
+
 m.reply(wait)
 
-let res = await fetch(`https://api.xteam.xyz/randomimage/ppcouple?APIKEY=NezukoTachibana281207`)
+let res = await fetch(`https://saipulanuar.herokuapp.com/api/random/couple?apikey=${bearkey}`)
+
 let json = await res.json()
 
-conn.sendFile(m.chat, json.result.male, 'ppcowo.png', 'Cowok', m, false,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
+conn.sendFile(m.chat, json.result.ppcwo, 'ppcowo.png', 'Cowok', m, false,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
 
-conn.sendFile(m.chat, json.result.female, 'ppcewe.png', 'Cewek', m,false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+conn.sendFile(m.chat, json.result.ppcwe, 'ppcewe.png', 'Cewek', m,false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
-conn.sendButton(m.chat, 'Klik Button Di Bawah Untuk Dapat Kan Foto Nya', '© Ziv San', 'Get Again', '.ppcp', m)
+conn.sendButton(m.chat, 'Klik Button Di Bawah Untuk Dapat Kan Foto Nya', '© King Of Bear', 'Get Again', '.ppcp', m)
+
 }
+
 handler.help = ['ppcouple', 'ppcp']
+
 handler.tags = ['internet', 'anime']
+
 handler.command = /^(pp(cp|couple))$/i
+
 handler.limit = true
 
 module.exports = handler
+
