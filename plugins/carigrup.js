196214649a034662cd7f58729cb1358b42df2bda
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-06 21:18:08 +0700

    p

--- plugins/carigrup.js
+++ plugins/carigrup.js
@@ -15,7 +15,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
     title: titlink,
     body: wm,
     thumbnail: await(await fetch(image)).buffer(),
-    sourceUrl: 'https://chat.whatsapp.com/CnQV9lNcHuIENb8SQ9CaYP'
+    sourceUrl: 'https://youtu.be/lDSHQvws9N0'
      }}
   })
 }
